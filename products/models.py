881acb7087ad7b0cf920516ab7ff3c2498db5ac3
Ark005 <005ark@gmail.com>
2020-12-22 13:31:04 +0300

    F+Com+front_page

--- products/models.py
+++ products/models.py
@@ -14,6 +14,8 @@ class Category(models.Model):
 
 #Product Model
 class  Product(models.Model):
+
+   
     mainimage = models.ImageField(upload_to='products/', blank=True)
     name = models.CharField(max_length=300)
     slug = models.SlugField()
@@ -31,26 +33,54 @@ class  Product(models.Model):
         return reverse("mainapp:product", kwargs={
             'slug': self.slug
         })
-class Friend(models.Model):
-    # NICK NAME should be unique
-    nick_name = models.CharField(max_length=100, unique =  True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    likes = models.CharField(max_length = 250)
-    dob = models.DateField(auto_now=False, auto_now_add=False)
-    lives_in = models.CharField(max_length=150, null = True, blank = True)
+
+    def result (self):
+        res = friend.tirazh 
+        return res
+
+class Option(models.Model):
+
+   
+    mainimage = models.ImageField(upload_to='products/', blank=True)
+    name = models.CharField(max_length=300)
+    slug = models.SlugField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    preview_text = models.TextField(max_length=200, verbose_name='Preview Text')
+    detail_text = models.TextField(max_length=1000, verbose_name='Detail Text')
+    
 
     def __str__(self):
-        return self.nick_name
+        return self.name
 
-# class Friend(models.Model):
-#     # NICK NAME should be unique
-#     nick_name = models.CharField(max_length=100, unique =  True)
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     likes = models.CharField(max_length = 250)
-#     dob = models.DateField(auto_now=False, auto_now_add=False)
-#     lives_in = models.CharField(max_length=150, null = True, blank = True)
 
-#     def __str__(self):
-#         return self.first_name + self.last_name
+    def get_absolute_url(self):
+        return reverse("mainapp:product", kwargs={
+            'slug': self.slug
+        })
+
+
+
+        
+class Friend(models.Model):
+
+
+    first_name = models.CharField(max_length=100)
+    tirazh = models.IntegerField(null=False)
+    dob = models.DateField(auto_now=False, auto_now_add=False)
+  
+
+
+   
+    def save(self, *args, **kwargs):
+        self.__class__.objects.exclude(id=self.id).delete()
+        super(Friend, self).save(*args, **kwargs)
+ 
+    @classmethod
+    def load(cls):
+        try:
+            return cls.objects.get()
+        except cls.DoesNotExist:
+            return cls()
+    
+    
+   
